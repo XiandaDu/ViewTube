d7934c7b68978f1d9dbdf751f2f18ca09780c6b0
XiandaDu <283149519du@gmail.com>
2024-02-29 23:44:20 -0500

    feat: navbar almost created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Box } from '@mui/material'
-import Navbar from "./Components/Navbar"
-import Feed from "./Components/Feed"
-import VideoDetail from "./Components/VideoDetail"
-import ChannelDetail from './Components/channelDetail';
-import SearchFeed from "./Components/SearchFeed"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-        <Box sx={{backgroundColor: '#000'}}>
-            <Navbar/>
-            <Routes>
-                <Route path="/" exact element={ <Feed/> }/>
-                <Route path="/video/:id" element={ <VideoDetail/> }/>
-                <Route path="/channel/:id" element={ <ChannelDetail/> }/>
-                <Route path="/search/:id" element={ <SearchFeed/> }/>
-            </Routes>
-        </Box>
-    </BrowserRouter>
-  )
-}
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './Components';
 
-export default App
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#F00' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
