f4cbb3de0e77b88a51cd1acc9fecf5a60b4cf96d
XiandaDu <283149519du@gmail.com>
2024-03-03 15:58:54 -0500

    feat: videos created, start API data fetching

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect} from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar'
+import { Sidebar, Videos } from './'
 
 const Feed = () => {
   return (
@@ -17,8 +17,9 @@ const Feed = () => {
             New
             <span style={{ color: '#F31503' }}>Videos</span>
         </Typography>
-
         
+        <Videos videos={[]}/>
+
       </Box>
     </Stack>
   )
