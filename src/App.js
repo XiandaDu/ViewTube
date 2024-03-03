4f31b7227195659b4143f65a267b93faaa346937
XiandaDu <283149519du@gmail.com>
2024-03-03 18:32:35 -0500

    feat: fetchAPI functionality added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './Components';
 
-const App = () => (
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: '#F00' }}>
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
-      </Routes>
-    </Box>
-  </BrowserRouter>
-);
+const App = () => {
+  return(
+    <BrowserRouter>
+        <Box sx={{ backgroundColor: '#F00' }}>
+        <Navbar />
+        <Routes>
+            <Route exact path='/' element={<Feed />} />
+            <Route path='/video/:id' element={<VideoDetail />} />
+            <Route path='/channel/:id' element={<ChannelDetail />} />
+            <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+        </Box>
+    </BrowserRouter>
+  )
+};
 
 export default App;
