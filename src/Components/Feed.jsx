7124992495848740106b1683ebab781687f78177
XiandaDu <283149519du@gmail.com>
2024-03-10 17:46:34 -0400

    feat: video player and video details

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,25 +1,25 @@
-import { useState, useEffect} from 'react'
-import { Box, Stack, Typography } from '@mui/material'
-import { Sidebar, Videos } from './'
-import { fetchFromAPI } from '../utils/fetchFromAPI'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState("New")
-  const [videos, setVideos] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
 
   useEffect(() => {
+    setVideos(null);
+
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-    .then((data) => setVideos(data.items))
-  }, [selectedCategory])
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <Sidebar
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
           Copyright © 2022 JSM Media
         </Typography>
@@ -30,11 +30,10 @@ const Feed = () => {
           {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
 
-        <Videos videos={videos}/>
-
+        <Videos videos={videos} />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
